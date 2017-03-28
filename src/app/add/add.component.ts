0bf82319df5d3ff834b8db839bb3f0e1cb67f4d7
Alexander Linkov <alexander.linkov@rtlabs.ru>
2017-03-28 10:44:43 +0400

    Unnecessary elements deprecated

--- src/app/add/add.component.ts
+++ src/app/add/add.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {Http, Response} from "@angular/http";
 
 @Component({
   selector: 'app-add',
@@ -8,55 +7,13 @@ import {Http, Response} from "@angular/http";
 })
 export class AddComponent implements OnInit {
 
-  cities: Array<string> = ["Самара", "Москва", "Санкт-петербург", "Тверь", "Казань", "Уфа", "Новосибирск", "Нижний Новгород", "Тольятти", "Сочи", "Владивосток", "Красноярск", "Архангельск"];
-  selectedCity: string;
-  task: any = {
-    Body: "",
-    Loc: "",
-    Deadline: null,
-    Urgent: false,
-    Night: false,
-    Personal: false,
-    Price: null,
-    Status: "new"
-  };
+  constructor() {
 
-
-  constructor(private http: Http) {
-
-  }
-
-  setLocation(city){
-    console.log('location is changed for '+city);
-    this.task.Loc = city;
-  }
-
-  unselect(event){
-    event.preventDefault();
-    event.currentTarget.blur();
   }
 
   randomDate(start, end) {
     return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
   }
-
-  GENrandomDate() {
-    this.task.Deadline = this.randomDate(new Date(2012, 0, 1), new Date()).getTime();
-    console.log(this.randomDate(new Date(2012, 0, 1), new Date()));
-  }
-
-  addTask() {
-    this.http.post('http://localhost:3000/tasks/', this.task).map((res: Response) => res.json()).subscribe(
-      (res: any) => {
-        console.log(res);
-        alert('A new task has been added');
-      },
-      err => {
-        console.log(err);
-      }
-    );
-  }
-
 
   ngOnInit() {
   }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Http, Response } from "@angular/http";
-import { Injectable } from '@angular/core';
-import 'rxjs/add/operator/map';
-import {CounterRefreshService} from "../service/counter-refresh.service";
 
 @Component({
   selector: 'app-tasks',
@@ -10,43 +6,13 @@ import {CounterRefreshService} from "../service/counter-refresh.service";
   styleUrls: ['./tasks.component.scss']
 })
 
-@Injectable()
 export class TasksComponent implements OnInit {
 
-  public tasksCounter: any = {
-    'new':0,
-    'active':0,
-    'pending':0,
-    'finished':0,
-    'canceled':0,
-    'drafts':0
-  }
+  constructor() {
 
-  constructor(public http: Http,
-              private _refreshCounter: CounterRefreshService) {
-    _refreshCounter.changeEmitted$.subscribe(
-      type => {
-        this.tasksCounter[type]--;
-      });
-  }
-
-  requestData() {
-    this.http.get('http://localhost:3000/tasks').map((res: Response) => res.json()).subscribe(
-      (res: any) => {
-        for (let task of res) {
-          this.tasksCounter[task.Status]++;
-        }
-      },
-      err => {
-        console.log(err);
-        //this.processingErrorStatus(err, callbackErr);
-      }
-    );
   }
 
   ngOnInit() {
-
-    this.requestData();
 
   }
 
