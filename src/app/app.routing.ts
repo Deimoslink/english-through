eb5f7152819e400e1ed78ab8870dd547bd3a9092
Alexander Linkov <alexander.linkov@rtlabs.ru>
2017-04-04 16:49:24 +0400

    Main field applied

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -13,21 +13,13 @@ import {AddComponent} from "./add/add.component";
 
 
 const APP_ROUTES: Routes = [
-  { path: 'tasks', component: TasksComponent, canActivate: [LoggedInGuard],
-    children: [
-      {path: 'new', component: TableComponent},
-      {path: 'active', component: TableComponent},
-      {path: 'canceled', component: TableComponent},
-      {path: 'finished', component: TableComponent},
-      {path: 'pending', component: TableComponent},
-      {path: 'drafts', component: TableComponent}
-    ]},
+  { path: 'tasks', component: TasksComponent, canActivate: [LoggedInGuard] },
   { path: 'statistics', component: StatisticsComponent, canActivate: [LoggedInGuard] },
   { path: 'company', component: CompanyComponent, canActivate: [LoggedInGuard] },
   { path: 'finance', component: FinanceComponent, canActivate: [LoggedInGuard] },
   { path: 'add', component: AddComponent, canActivate: [LoggedInGuard] },
   { path: 'login', component: LoginComponent},
-  { path: '', redirectTo: 'tasks/active', pathMatch: 'full' }
+  { path: '', redirectTo: 'tasks', pathMatch: 'full' }
 ];
 
 export const routing:ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
--- src/app/company/company.component.ts
+++ src/app/company/company.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   })
   export class CompanyComponent implements OnInit {
 
+  public courses: any = [1,2,3,4];
+
   constructor() {}
 
   ngOnInit() {
